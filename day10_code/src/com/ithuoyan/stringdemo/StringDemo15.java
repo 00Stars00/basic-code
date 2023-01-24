baf7502fa3247407a46f6c3e27123c8e1de80514
00Stars00 <87743646+00Stars00@users.noreply.github.com>
2023-01-24 23:35:55 +0800

    练习

--- day10_code/src/com/ithuoyan/stringdemo/StringDemo15.java
+++ day10_code/src/com/ithuoyan/stringdemo/StringDemo15.java
@@ -1,5 +1,34 @@
 package com.ithuoyan.stringdemo;
 
 public class StringDemo15 {
+    public static void main(String[] args) {
+        String strA = "abcde";
+        String strB = "abc";
+
+        boolean result = check(strA, strB);
+        System.out.println(result);
+
+
+    }
+
+
+    public static boolean check(String strA, String strB) {
+        if (strA.length() != strB.length()) {
+            return false;
+        }
+        for (int i = 0; i < strA.length(); i++) {
+            strA = rotate(strA);
+            if (strA.equals(strB)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static String rotate(String str) {
+        String strA = str.substring(0, 1);
+        String strB = str.substring(1);
+        return strB + strA;
+    }
 
 }
---
+++ day10_code/src/com/ithuoyan/stringdemo/StringDemo16.java
@@ -0,0 +1,7 @@
+package com.ithuoyan.stringdemo;
+
+public class StringDemo16 {
+    public static void main(String[] args) {
+
+    }
+}
