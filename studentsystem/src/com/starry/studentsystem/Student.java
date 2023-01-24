395f0c90d202d6d9ba5327d25eb5c6c15ee5175d
00Stars00 <87743646+00Stars00@users.noreply.github.com>
2023-01-25 02:39:08 +0800

    学生管理系统

--- studentsystem/src/com/starry/studentsystem/Student.java
+++ studentsystem/src/com/starry/studentsystem/Student.java
@@ -4,13 +4,28 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * 学生类
+ */
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class Student {
+    /**
+     * 学号
+     */
     private String id;
+    /**
+     * 姓名
+     */
     private String name;
+    /**
+     * 年龄
+     */
     private int age;
+    /**
+     * 地址
+     */
     private String address;
 
 
