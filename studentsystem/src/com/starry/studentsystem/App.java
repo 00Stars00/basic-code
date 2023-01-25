18c840eabe2eed828e7956c78bd50d2ece647a64
00Stars00 <87743646+00Stars00@users.noreply.github.com>
2023-01-25 12:51:15 +0800

    学生管理系统

--- studentsystem/src/com/starry/studentsystem/App.java
+++ studentsystem/src/com/starry/studentsystem/App.java
@@ -351,7 +351,7 @@ public class App {
      * @return 验证码
      */
     private static String getCode() {
-        ArrayList<Character> list = new ArrayList<Character>();
+        ArrayList<Character> list = new ArrayList<>();
         for (int i = 0; i < 26; i++) {
             list.add((char) ('a' + i));
             list.add((char) ('A' + i));
