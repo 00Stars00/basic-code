48c0cb0ead6f8846d7f9ba015affd384d636c1c9
00Stars00 <87743646+00Stars00@users.noreply.github.com>
2023-02-02 01:29:47 +0800

    拼图游戏单机版 V1.0

---
+++ puzzlegame/src/App.java
@@ -0,0 +1,9 @@
+import com.start.ui.GameJFrame;
+import com.start.ui.LoginJFrame;
+import com.start.ui.RegisterJFrame;
+
+public class App {
+    public static void main(String[] args) {
+        new GameJFrame();
+    }
+}
---
+++ puzzlegame/src/com/start/ui/GameJFrame.java
@@ -0,0 +1,25 @@
+package com.start.ui;
+
+import javax.swing.*;
+
+public class GameJFrame extends JFrame {
+    public GameJFrame() {
+        // 设置窗口大小
+        this.setSize(603,680);
+
+        // 设置窗口标题
+        this.setTitle("拼图游戏单机版 V1.0");
+
+        // 设置界面置顶
+        this.setAlwaysOnTop(true);
+
+        // 设置窗口居中
+        this.setLocationRelativeTo(null);
+
+        // 设置窗口关闭时退出程序
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // 设置窗口可见
+        this.setVisible(true);
+    }
+}
